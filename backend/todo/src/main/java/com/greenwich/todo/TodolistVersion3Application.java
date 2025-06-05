660a4c0151c84269a62a7c338656b01a52408d7d
phamthang44 <Ducthang4342@gmail.com>
2025-06-05 16:34:49 +0700

    src for backend project

---
+++ backend/todo/src/main/java/com/greenwich/todo/TodolistVersion3Application.java
@@ -0,0 +1,13 @@
+package com.greenwich.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistVersion3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistVersion3Application.class, args);
+	}
+
+}
---
+++ backend/todo/src/test/java/com/greenwich/todo/TodolistVersion3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenwich.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistVersion3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
